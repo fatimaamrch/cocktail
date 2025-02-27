1ac8c266e248f3ed867ba883369a05b25b6e2858
allanthieryd <allan.td@free.fr>
2025-02-27 17:04:43 +0100

    Fixed Clear filter

--- app/src/main/java/com/example/myapplication/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/SearchScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,12 +10,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Clear
@@ -26,69 +26,41 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
+import com.example.myapplication.R
 import com.example.myapplication.model.PictureBean
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.viewmodel.MainViewModel
 
 @Composable
-fun SearchScreen(
-    modifier: Modifier = Modifier,
-    mainViewModel: MainViewModel = MainViewModel()
-) {
-    var searchText by remember { mutableStateOf("") }
+fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
 
-    Column(
-        modifier = modifier
-            .background(Color.LightGray)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        println("SearchScreen()")
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
 
-        TextField(
-            value = searchText,
-            onValueChange = { newValue ->
-                searchText = newValue
-                mainViewModel.search(newValue) // Ne recharge pas les données, filtre seulement
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    tint = MaterialTheme.colorScheme.primary,
-                    contentDescription = null
-                )
-            },
-            singleLine = true,
-            label = { Text("Votre recherche ici") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 56.dp)
-                .padding(8.dp)
-        )
+        val searchText = remember { mutableStateOf("") }
 
-        Spacer(Modifier.size(8.dp))
+        SearchBar(searchText = searchText)
 
-        // Observation de la liste filtrée
-        val list = mainViewModel.filteredDataList.collectAsStateWithLifecycle().value
+        // Observation
+        val list = mainViewModel.dataList.collectAsStateWithLifecycle().value.filter { it.title.contains(searchText.value, true) }
 
-        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(list) { item ->
-                PictureRowItem(data = item)
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.weight(5f)) {
+            items(list.size) {
+                PictureRowItem(data = list[it])
             }
         }
 
@@ -100,8 +72,7 @@ fun SearchScreen(
         ) {
             Button(
                 onClick = {
-                    searchText = ""
-                    mainViewModel.clearFilter()
+                    searchText.value = ""
                 },
                 modifier = Modifier.weight(1f).padding(end = 8.dp)
             ) {
@@ -111,11 +82,11 @@ fun SearchScreen(
                     contentDescription = null
                 )
                 Spacer(modifier = Modifier.size(4.dp))
-                Text("Clear filter")
+                Text("${stringResource(R.string.clear_filter)} Clear filter")
             }
 
             Button(
-                onClick = { mainViewModel.clearFilter() },
+                onClick = { mainViewModel.loadData() },
                 modifier = Modifier.weight(1f)
             ) {
                 Icon(
@@ -130,47 +101,88 @@ fun SearchScreen(
     }
 }
 
+@Composable
+fun SearchBar(modifier: Modifier = Modifier, searchText: MutableState<String>) {
+
+    TextField(
+        value = searchText.value, //Valeur affichée
+        onValueChange = {newValue:String -> searchText.value = newValue }, //Nouveau texte entrée
+        leadingIcon = { //Image d'icone
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        singleLine = true,
+        label = { //Texte d'aide qui se déplace
+            Text("Enter text")
+            //Pour aller le chercher dans string.xml
+            //Text(stringResource(R.string.placeholder_search))
+        },
+        //placeholder = { //Texte d'aide qui disparait
+        //Text("Recherche")
+        //},
+
+        //keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search), // Définir le bouton "Entrée" comme action de recherche
+        //keyboardActions = KeyboardActions(onSearch = {onSearchAction()}), // Déclenche l'action définie
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
+}
 
 @OptIn(ExperimentalGlideComposeApi::class)
-@Composable
+@Composable //Composable affichant 1 PictureBean
 fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
-    var expanded by remember { mutableStateOf(false) }
 
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .background(MaterialTheme.colorScheme.surface)
-            .clickable { expanded = !expanded } // Toggle l'état d'affichage complet
-    ) {
-        // Affichage de l'image avec Glide
+    var expended by remember { mutableStateOf(false) }
+
+    Row(modifier = modifier
+        .background(MaterialTheme.colorScheme.tertiaryContainer)
+        .fillMaxWidth()) {
+
+        //Permission Internet nécessaire
         GlideImage(
             model = data.url,
-            contentDescription = "Image de ${data.title}",
-            loading = placeholder(android.R.drawable.stat_sys_download),
-            failure = placeholder(android.R.drawable.stat_notify_error),
-            contentScale = ContentScale.Crop,
+            //Pour aller le chercher dans string.xml
+            //contentDescription = getString(R.string.picture_of_cat),
+            //En dur
+            contentDescription = "une photo de chat",
+            // Image d'attente. Permet également de voir l'emplacement de l'image dans la Preview
+            loading = placeholder(R.mipmap.ic_launcher_round),
+            // Image d'échec de chargement
+            failure = placeholder(R.mipmap.ic_launcher),
+            contentScale = ContentScale.Fit,
+            //même autres champs qu'une Image classique
             modifier = Modifier
-                .size(100.dp)
-                .padding(end = 8.dp)
+                .heightIn(max = 100.dp) //Sans hauteur il prendra tous l'écran
+                .widthIn(max = 100.dp)
         )
 
-        // Affichage du titre et de la description
-        Column {
+
+
+        Column(modifier = Modifier.padding(4.dp).fillMaxWidth().clickable {
+            expended = !expended
+        }) {
+
+
             Text(text = data.title, style = MaterialTheme.typography.titleLarge)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = if (expanded) data.longText else data.longText.take(20) + "...",
-                fontSize = 12.sp,
-                color = MaterialTheme.colorScheme.primary
+
+            val text = if(expended) data.longText else (data.longText.take(20) + "...")
+
+            Text(text = text,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                modifier = Modifier.animateContentSize()
             )
         }
     }
 }
 
-
 @Preview(showBackground = true, showSystemUi = true)
-@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES, locale = "fr")
 @Composable
 fun SearchScreenPreview() {
     //Il faut remplacer NomVotreAppliTheme par le thème de votre application
--- app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.kt
@@ -1,76 +1,86 @@
 package com.example.myapplication.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myapplication.model.PictureBean
+import com.example.myapplication.model.WeatherRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+fun main() {
+    val viewModel = MainViewModel()
+    viewModel.loadWeathers("")
+
+    while (viewModel.runInProgress.value) {
+        println("Attente...")
+        Thread.sleep(500)
+    }
+
+    //Affichage de la liste (qui doit être remplie) contenue dans la donnée observable
+    println("List : ${viewModel.dataList.value}")
+    println("ErrorMessage : ${viewModel.errorMessage.value}")
+}
 
 const val LONG_TEXT = """Le Lorem Ipsum est simplement du faux texte employé dans la composition
     et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard
     de l'imprimerie depuis les années 1500"""
 
 open class MainViewModel : ViewModel() {
-    private val fullDataList = MutableStateFlow(emptyList<PictureBean>())
-    private val _filteredDataList = MutableStateFlow(emptyList<PictureBean>())
-    val filteredDataList: StateFlow<List<PictureBean>> get() = _filteredDataList
-
+    //MutableStateFlow est une donnée observable
+    val dataList = MutableStateFlow(emptyList<PictureBean>())
     val runInProgress = MutableStateFlow(false)
     val errorMessage = MutableStateFlow("")
 
-//    fun loadWeathers(cityName: String) {
-//        runInProgress.value = true
-//        viewModelScope.launch(Dispatchers.IO) {
-//            try {
-//                val weatherList = WeatherRepository.loadWeathers(cityName)
-//                val mappedList = weatherList.map { city ->
-//                    PictureBean(
-//                        id = city.id.toInt(),
-//                        url = city.weather.firstOrNull()?.icon ?: "",
-//                        title = city.name,
-//                        longText = """
-//                           Il fait ${city.main.temp}° à ${city.name} (id=${city.id}) avec un vent de ${city.wind.speed} m/s
-//                           - Description : ${city.weather.getOrNull(0)?.description ?: "-"}
-//                           - Icône : ${city.weather.getOrNull(0)?.icon ?: "-"}
-//                       """.trimIndent()
-//                    )
-//                }
-//                fullDataList.value = mappedList
-//                _filteredDataList.value = mappedList
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//                errorMessage.value = e.message ?: "Une erreur est survenue"
-//            }
-//            runInProgress.value = false
-//        }
-//    }
+    fun loadWeathers(cityName: String) {
 
-    init {
+        runInProgress.value = true
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val weatherlist = WeatherRepository.loadWeathers(cityName)
+                dataList.value = weatherlist.map { city ->
+                    PictureBean(
+                        id = city.id.toInt(),
+                        url = city.weather.firstOrNull()?.icon ?: "",
+                        title = city.name,
+                        longText = """
+           Il fait ${city.main.temp}° à ${city.name} (id=${city.id}) avec un vent de ${city.wind.speed} m/s
+           -Description : ${city.weather.getOrNull(0)?.description ?: "-"}
+           -Icône : ${city.weather.getOrNull(0)?.icon ?: "-"}
+       """.trimIndent()
+                    )
+                }
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+                errorMessage.value = e.message ?: "Une erreur est survenue"
+            }
+
+            runInProgress.value = false
+        }
+
+
+        println("fin")
+
+
+    }
+
+    init {//Création d'un jeu de donnée au démarrage
         loadFakeData()
     }
 
-    fun loadFakeData(runInProgress: Boolean = false, errorMessage: String = "") {
+    fun loadFakeData(runInProgress :Boolean = false, errorMessage:String = "" ) {
         this.runInProgress.value = runInProgress
         this.errorMessage.value = errorMessage
-        val fakeData = listOf(
-            PictureBean(1, "https://picsum.photos/200", "ABCD", LONG_TEXT),
+        dataList.value = listOf(PictureBean(1, "https://picsum.photos/200", "ABCD", LONG_TEXT),
             PictureBean(2, "https://picsum.photos/201", "BCDE", LONG_TEXT),
             PictureBean(3, "https://picsum.photos/202", "CDEF", LONG_TEXT),
             PictureBean(4, "https://picsum.photos/203", "EFGH", LONG_TEXT)
-        ).shuffled()
-
-        fullDataList.value = fakeData
-        _filteredDataList.value = fakeData
+        ).shuffled() //shuffled() pour avoir un ordre différent à chaque appel
     }
 
-    fun search(query: String) {
-        _filteredDataList.value = if (query.isBlank()) {
-            fullDataList.value
-        } else {
-            fullDataList.value.filter { it.title.contains(query, ignoreCase = true) }
-        }
-    }
+    fun loadData() {
 
-    fun clearFilter() {
-        _filteredDataList.value = fullDataList.value
     }
 }
