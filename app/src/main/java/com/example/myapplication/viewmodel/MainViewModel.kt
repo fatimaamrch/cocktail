13c865af5ae2127781183c735468b98a2f984851
allanthieryd <allan.td@free.fr>
2025-02-27 16:23:09 +0100

    Better SearchScreen

--- app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.kt
@@ -1,13 +1,9 @@
 package com.example.myapplication.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.myapplication.model.PictureBean
-import com.example.myapplication.model.WeatherRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 const val LONG_TEXT = """Le Lorem Ipsum est simplement du faux texte employé dans la composition
     et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard
@@ -21,32 +17,32 @@ open class MainViewModel : ViewModel() {
     val runInProgress = MutableStateFlow(false)
     val errorMessage = MutableStateFlow("")
 
-    fun loadWeathers(cityName: String) {
-        runInProgress.value = true
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val weatherList = WeatherRepository.loadWeathers(cityName)
-                val mappedList = weatherList.map { city ->
-                    PictureBean(
-                        id = city.id.toInt(),
-                        url = city.weather.firstOrNull()?.icon ?: "",
-                        title = city.name,
-                        longText = """
-                           Il fait ${city.main.temp}° à ${city.name} (id=${city.id}) avec un vent de ${city.wind.speed} m/s
-                           - Description : ${city.weather.getOrNull(0)?.description ?: "-"}
-                           - Icône : ${city.weather.getOrNull(0)?.icon ?: "-"}
-                       """.trimIndent()
-                    )
-                }
-                fullDataList.value = mappedList
-                _filteredDataList.value = mappedList
-            } catch (e: Exception) {
-                e.printStackTrace()
-                errorMessage.value = e.message ?: "Une erreur est survenue"
-            }
-            runInProgress.value = false
-        }
-    }
+//    fun loadWeathers(cityName: String) {
+//        runInProgress.value = true
+//        viewModelScope.launch(Dispatchers.IO) {
+//            try {
+//                val weatherList = WeatherRepository.loadWeathers(cityName)
+//                val mappedList = weatherList.map { city ->
+//                    PictureBean(
+//                        id = city.id.toInt(),
+//                        url = city.weather.firstOrNull()?.icon ?: "",
+//                        title = city.name,
+//                        longText = """
+//                           Il fait ${city.main.temp}° à ${city.name} (id=${city.id}) avec un vent de ${city.wind.speed} m/s
+//                           - Description : ${city.weather.getOrNull(0)?.description ?: "-"}
+//                           - Icône : ${city.weather.getOrNull(0)?.icon ?: "-"}
+//                       """.trimIndent()
+//                    )
+//                }
+//                fullDataList.value = mappedList
+//                _filteredDataList.value = mappedList
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//                errorMessage.value = e.message ?: "Une erreur est survenue"
+//            }
+//            runInProgress.value = false
+//        }
+//    }
 
     init {
         loadFakeData()
